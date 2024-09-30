4fa24d9878ddeaa6f17f8f8d781ddb79a6402441
Vujadin Dagovic <vujke456@gmail.com>
2024-09-30 21:59:14 +0200

    fixing hero image in index.css

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -7,11 +7,6 @@ const NotFoundPage = () => {
       className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
       style={{ backgroundImage: `url('/404.png')` }}
     >
-      <header className="absolute top-0 left-0 p-4 bg-black w-full ">
-        <Link to={"/"}>
-          <img src="/netflix-logo.png" alt="Netflix" className="h-8" />
-        </Link>
-      </header>
       <main className="text-center error-page--content z-10">
         <h1 className="text-7xl font-semibold mb-4">Lost your way?</h1>
         <p className="mb-6 text-xl">
